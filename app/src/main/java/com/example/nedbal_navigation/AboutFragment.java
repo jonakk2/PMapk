b3fab8f059e840f5f528669776fe6830d4f64e81
jonakk2 <j_nedbal@utb.cz>
2023-01-23 19:45:48 +0100

    zmena_about

--- app/src/main/java/com/example/nedbal_navigation/AboutFragment.java
+++ app/src/main/java/com/example/nedbal_navigation/AboutFragment.java
@@ -4,19 +4,29 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class AboutFragment extends Fragment {
-
+    TextView api;
+    TextView icon;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        api = view.findViewById(R.id.api);
+        api.setMovementMethod(LinkMovementMethod.getInstance());
+
+        icon = view.findViewById(R.id.icon);
+        icon.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return view;
     }
 }
